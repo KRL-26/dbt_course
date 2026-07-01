a9321873d29a3addf359d7119d80f87b118d4c02
Kruizlem <kruizlem99@gmail.com>
2026-07-01 18:16:31 +0200

    Añadimos modificaciones hechas en el esquema dev: modificacion en la colimna price_str del modelo dim_lisintgs_w_hosts y dim_listings_cleansed. Ademas, añadimos comentarios en el GitHub Actions pr-slim-ci

--- airbnb/models/dim/dim_listings_w_hosts.sql
+++ airbnb/models/dim/dim_listings_w_hosts.sql
@@ -13,7 +13,8 @@ SELECT
     l.listing_name,
     l.room_type,
     l.minimum_nights,
-    l.price AS price,                                       --Price_usd: Ejemplo de cambio para comparar versiones entre prod vs dev
+    l.price AS price,  
+    l.price_str,                                     --Price_usd: Ejemplo de cambio para comparar versiones entre prod vs dev
     l.host_id,
     h.host_name,
     h.is_superhost AS host_is_superhost,
